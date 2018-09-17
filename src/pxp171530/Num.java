efe0e5c16e260b2b619752fa15d56c0d6f2a6979
Pushpita <pooja.p108@gmail.com>
2018-09-16 21:23:56 -0500

    By2 and convertbase

--- src/pxp171530/Num.java
+++ src/pxp171530/Num.java
@@ -21,10 +21,10 @@ public class Num implements Comparable<Num> {
 			this.isNegative = true;
 			s = s.substring(1);
 		}
-		this.len = (int) Math.ceil(((s.length() + 1) / Math.log10(this.base)) + 1);
+		this.len = s.length();
 		arr = new long[this.len];
 		for (int i = len - 1; i > 0; i--) {
-			arr[len - 1 - i] = s.charAt(i);
+			arr[len - 1 - i] = s.charAt(i) - '0';
 		}
 	}
 
@@ -32,47 +32,51 @@ public class Num implements Comparable<Num> {
 		this(x + "");
 	}
 
-	public static Num add(Num a, Num b) {
-
-		String sumStr = "";
+	public static Num add(Num a, Num b) throws Exception {
 
 		// Define positive/negative
 		if (a.isNegative && !b.isNegative && a.compareTo(b) > 0) {
-			return subtract(b, a, sumStr);
+			return subtractHelper(b, a);
 		}
 		if (!a.isNegative && b.isNegative && a.compareTo(b) < 0) {
-			return subtract(b, a, sumStr);
+			return subtractHelper(b, a);
 		}
-		if (a.isNegative && b.isNegative) {
-			sumStr += "-";
-		}
+
 		// perform add and return
-		return add(a, b, sumStr);
+		Num result = addHelper(a, b);
+		if (a.isNegative && b.isNegative) {
+			result.isNegative = a.isNegative;
+		}
+		return result;
 
 	}
 
-	public static Num add(Num a, Num b, String sumStr) {
+	public static Num addHelper(Num a, Num b) throws Exception {
 
+		String sumStr = "";
+		if (a.base != b.base) {
+			throw new Exception();
+		}
 		// perform addition
 		int i = 0, j = 0;
 		long carry = 0, sum = 0;
 		while (i < a.len && j < b.len) {
 			sum = a.arr[i] + b.arr[j] + carry;
-			sumStr += sum % base;
-			carry = sum / base;
+			sumStr += sum % a.base;
+			carry = sum / a.base;
 			i++;
 			j++;
 		}
 		while (i < a.len) {
 			sum = a.arr[i] + carry;
-			sumStr += sum % base;
-			carry = sum / base;
+			sumStr += sum % a.base;
+			carry = sum / a.base;
 			i++;
 		}
 		while (j < b.len) {
 			sum = b.arr[j] + carry;
-			sumStr += sum % base;
-			carry = sum / base;
+			sumStr += sum % a.base;
+			carry = sum / a.base;
 			j++;
 		}
 		if (carry != 0) {
@@ -81,19 +85,20 @@ public class Num implements Comparable<Num> {
 		return new Num(sumStr);
 	}
 
-	public static Num subtract(Num a, Num b) {
+	public static Num subtract(Num a, Num b) throws Exception {
 		String subStr = "";
 		if (a.isNegative != b.isNegative) {
-			Num result = add(a, b, subStr);
+			Num result = addHelper(a, b);
 			result.isNegative = a.isNegative;
 			return result;
 		}
 
-		return subtract(a, b, subStr);
-
+		Num result = subtractHelper(a, b);
+		return result;
 	}
 
-	public static Num subtract(Num a, Num b, String subStr) {
+	public static Num subtractHelper(Num a, Num b) {
+		String subStr = "";
 		if (a.compareTo(b) > 0) {
 
 		}
@@ -140,21 +145,78 @@ public class Num implements Comparable<Num> {
 
 	// Return number to a string in base 10
 	public String toString() {
-		return null;
+		Num base10;
+		String result = "";
+
+		try {
+			base10 = this.convertToBase10();
+			for (int i = 0; i < base10.len - 1; i++) {
+				result = base10.arr[i] + result;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	public long base() {
 		return base;
 	}
 
+	public Num convertToBase10() throws Exception {
+		// convert to decimal - horner's method
+		Num inBase10 = new Num(0);
+		for (int i = 0; i <= this.len - 1; i++) {
+			Num part = new Num((long) (this.arr[i] * Math.pow(10, i))); // in base 10 so we can call the constructor
+			add(inBase10, part);
+		}
+		inBase10.base = 10;
+		return inBase10;
+	}
+
 	// Return number equal to "this" number, in base=newBase
-	public Num convertBase(int newBase) {
-		return null;
+	public Num convertBase(int newBase) throws Exception {
+		Num inNewBase = this;
+		// convert to decimal
+		if (this.base != 10) {
+			inNewBase = this.convertToBase10();
+		}
+		if (newBase != 10) {
+			// convert to new base from decimal
+			Num newbase = new Num(newBase);
+			int size = (int) Math.ceil(((inNewBase.len + 1) / Math.log10(newBase)) + 1);
+			long[] newNumArr = new long[size];
+			int i = 0;
+			while (inNewBase.len == 1 && inNewBase.arr[0] == 0) {
+				newNumArr[i] = mod(inNewBase, newbase).arr[0];
+				inNewBase = divide(inNewBase, newbase);
+				i++;
+			}
+		}
+		inNewBase.base = newBase;
+		return inNewBase; // call truncate here
 	}
 
 	// Divide by 2, for using in binary search
 	public Num by2() {
-		return null;
+		long[] newNum;
+		if (this.base == 2) {
+			newNum = new long[this.len - 1];
+			for (int i = 1; i < len; i++) {
+				newNum[i - 1] = this.arr[i];
+			}
+
+		} else {
+			newNum = new long[this.len];
+			long carry = 0;
+			for (int i = 0; i < len; i++) {
+				long num = (carry + this.arr[this.len - 1 - i]);
+				carry = num % 2;
+				newNum[i] = num / 2;
+			}
+		}
+		return new Num(newNum, this.base);
 	}
 
 	// Evaluate an expression in postfix and return resulting number
@@ -171,7 +233,7 @@ public class Num implements Comparable<Num> {
 		return null;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Num x = new Num(999);
 		Num y = new Num("8");
 		Num z = Num.add(x, y);
