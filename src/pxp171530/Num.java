9a6b4a46cccf8b27f2fd5c55d095e25d941a6724
Pushpita <pooja.p108@gmail.com>
2018-09-17 22:57:33 -0500

    printlist added

--- src/pxp171530/Num.java
+++ src/pxp171530/Num.java
@@ -223,6 +223,11 @@ public class Num  implements Comparable<Num> {
     // For example, if base=100, and the number stored corresponds to 10965,
     // then the output is "100: 65 9 1"
     public void printList() {
+		System.out.print(this.base + ": ");
+    		for(int i=0; i<len; i++) {
+			System.out.print(this.arr[i] + " ");
+    		}
+		System.out.println();
     }
     
     // Return number to a string in base 10
